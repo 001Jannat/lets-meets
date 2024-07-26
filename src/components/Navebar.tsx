a374fd76f11ecb29196e0bfbfe4b4e1cc2bcf895
Jannat Khatoon <jkhatoon276@gmail.com>
2024-07-26 21:29:01 +0530

    sign-in sign-up process by clerk

--- src/components/Navebar.tsx
+++ src/components/Navebar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { SignedIn, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 import MobileNav from './MobileNav';
 
@@ -20,10 +20,10 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        {/* <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn> */}
-
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+  
         <MobileNav />
       </div>
     </nav>
